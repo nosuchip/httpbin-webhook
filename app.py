08597fa9504bc4cfcf44c12ef8799824264547c4
Alex Xela <nosuchip@gmail.com>
2021-04-28 20:16:08 +0700

    Replace SENDGRID username/password with API key. Upgrade stack to heroku-20

--- app.py
+++ app.py
@@ -8,19 +8,18 @@ import json
 from collections import OrderedDict
 
 app = Flask(__name__)
-app.config['MAIL_PASSWORD'] = os.getenv('SENDGRID_PASSWORD', 'r5mdhe6m7189')
-app.config['MAIL_USERNAME'] = os.getenv('SENDGRID_USERNAME', 'app45165158@heroku.com')
+app.config['MAIL_USERNAME'] = 'apikey'
+app.config['MAIL_PASSWORD'] = os.environ['SENDGRID_API_KEY']
+app.config['MAIL_USERNAME'] = 'apikey'
 app.config['MAIL_SERVER'] = "smtp.sendgrid.net"
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_DEFAULT_SENDER'] = "hear-service@hear.herokuapp.com"
-app.config['MAIL_DEBUG'] = 0
 app.config['MAIL_MAX_EMAILS'] = None
 app.config['MAIL_SUPPRESS_SEND'] = None
 
 mail = Mail(app)
-
 
 @app.route('/<path:path>', methods=['GET', 'OPTIONS', 'HEAD', 'POST', 'PUT', 'DELETE', 'TRACE', 'CONNECT'])
 def index(path=None):
